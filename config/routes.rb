79c4ea077dbc2e810deb734591ca0019de48afb2
Dan Crews <dancrews@me.com>
2013-10-24 22:10:07 -0400

    Added devise sign-in

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 CongressWatch::Application.routes.draw do
 
+  devise_for :users
   resources :legislators
   resources :pages
   resources :subjects
-  resources :users
 
 
 
@@ -18,7 +18,7 @@ CongressWatch::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   # You can have the root of your site routed with "root"
-  root 'legislators#index'
+  root :to => "home#index"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
