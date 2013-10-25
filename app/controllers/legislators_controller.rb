abbefcf1bb40c1f7aead0abfc14359daac294c33
Dan Crews <dancrews@me.com>
2013-10-25 10:09:24 -0400

    Added form for zipcode

--- app/controllers/legislators_controller.rb
+++ app/controllers/legislators_controller.rb
@@ -1,13 +1,23 @@
 class LegislatorsController < ApplicationController
   def index
-    @user = User.first
-    @zipcode = params[:zipcode] || "02134"
+    @zipcode = params[:id] || "02134"
     @legislators = Congress.legislators_locate(@zipcode)
   end
 
-  def create
-    @house_rep = @legislators[:representative]
-    @senator_sr = @legislators[:senior_senator]
-    @senator_jr = @legislators[:junior_senator]
+  def new
   end
+
+  def show
+  end
+
+
+  def create     
+    @legislator = Legislator.new(zipcode_params)     
+    if
+      @legislator.save format.html { redirect_to @legislator, notice: 'Legislators were
+        successfully created.' }
+      end
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through. def person_params params.require(: person). permit(: first_name, :last_name) end
 end
